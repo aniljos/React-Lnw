101cf41d7e4740b485c325d812f6d5b289d49dcf
aniljos <anil.jos@gmail.com>
2025-04-01 10:39:25 +0530

    Day 2 session 1

---
+++ the-awesome-app-ts/src/App.tsx
@@ -0,0 +1,30 @@
+
+
+
+function App() {
+
+  return (
+    <div className="container">
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">React-Vite</a>
+          <ul className="nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Counter</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Login</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Products</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+export default App
---
+++ the-awesome-app-ts/src/components/Message.tsx
@@ -0,0 +1,40 @@
+import {useEffect} from 'react';
+
+type MessageProps = {
+    text: string;
+    color?: string;
+}
+function Message(props: MessageProps){
+
+
+    // api call
+    console.log("props", props);
+
+    //useEffect(setUpFn, [dependencies])
+
+    //Mount -useEffect(setUpFn, [empty dependency array])
+    useEffect(() => {
+        console.log("Message component mounted...");
+
+        return () => {
+            console.log("Message component unmounted...");
+        }
+
+    }, [])
+
+
+    const myJSX = <div>Testing</div>
+    return (
+        <div style={{border : `2px solid ${props.color}`, margin: "3px", padding: "2px"}}>
+            <h3>Message: {props.text}</h3>
+            <p>This is a functional component</p>
+            <p>Expression: {5 + 7} </p>
+            <div>Expression: {myJSX} </div>
+
+            <p>Created at: {new Date().toLocaleString()}</p>
+        </div>
+    )
+}
+
+export default Message;
+
